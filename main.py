1ea22a0391940cbbc8582418a16b4e68fb5bede4
Danilo Bizarria <unifdaoliveira@fei.edu.br>
2022-04-09 18:14:19 -0300

    first attempt at formatting the output

--- main.py
+++ main.py
@@ -3,21 +3,26 @@ from runs import runs
 from thelongrun import thelongrun
 from testpoker import test
 
+
 def main():
-    for i in range(0, 26):
+    for i in range(1, 27):
         key = input()
         # remove quotes
         key = (key[1:])[:-1]
         # print(chave)
         # print(i)
 
-        print(i, end=": ")
-        # print(key, end=": ")
-        print("monobit", monobit(key), end=", ")
+        print("key", str(i) + ":")
+        indent = "  "
 
-        print("runs", runs(key))
+        # print(key, end=": ")
+        print(indent, "monobit:", monobit(key))
+
+        print(indent, "runs:", runs(key))
         
-        print("poker test", test(key))
+        print(indent, "poker test:", test(key))
         
-        print("thelongrun", thelongrun(key))
+        print(indent, "the long run:", thelongrun(key))
+
+
 main()
