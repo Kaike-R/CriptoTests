80689281cc468224bf1db763cdda12054a068f65
Danilo Bizarria <unifdaoliveira@fei.edu.br>
2022-04-10 12:46:05 -0300

    match output to given example

--- main.py
+++ main.py
@@ -6,44 +6,28 @@ from testpoker import test as pokertest
 
 def print_monobittest(key, indent, level):
     monobit_ok = monobittest(key)
-    num_monobits = countOnes(key)
-    print(indent * level, "monobit test:")
-    level += 1
-    print(indent * level, "passed:", monobit_ok)
-    print(indent * level, "number of monobits:", num_monobits)
+    print("monobit=" + str(monobit_ok))
 
     return monobit_ok
 
 
 def print_pokertest(key, indent, level):
     poker_ok = pokertest(key)
-    print(indent * level, "poker test:")
-    level += 1
-    print(indent * level, "passed:", poker_ok)
-    print(indent * level, "nibble counts:", [0] * 16)
+    print("poker_test=" + str(poker_ok))
 
     return poker_ok
 
 
 def print_runstest(key, indent, level):
     runs_ok = runstest(key)
-    print(indent * level, "runs test:")
-    level += 1
-    print(indent * level, "passed:", runs_ok)
-    print(indent * level, "runs counts:")
-    level += 1
-    runsCounts = countRuns(key)
-    for i in [0, 1]:
-        print(indent * level, str(i) + ":", runsCounts[i])
+    print("runs=" + str(runs_ok))
 
     return runs_ok
     
 
 def print_longruntest(key, indent, level):
     longrun_ok = longruntest(key)
-    print(indent * level, "long run test:")
-    level += 1
-    print(indent * level, "passed:", longrun_ok)
+    print("long_run=" + str(longrun_ok))
 
     return longrun_ok
 
@@ -58,7 +42,6 @@ def print_tests(key, indent, level):
     longrun_ok = print_longruntest(key, indent, level)
 
     all_ok = all([monobit_ok, poker_ok, runs_ok, longrun_ok])    
-    print(indent * level, "is random:", all_ok)
 
     return all_ok
 
@@ -71,22 +54,12 @@ def main():
         # remove quotes
         key = (key[1:])[:-1]
 
-        print("key", str(i) + ":")
+        print("chave=" + str(i))
         indent = "  "
         level = 1
 
-        is_random = print_tests(key, indent, level)
-
-        tests_results.append(is_random)
-        print("")
-
-    random_keys = []
-    for (index, is_random)  in enumerate(tests_results):
-        if is_random:
-            # from 0-based index to 1-based count
-            random_keys.append(index + 1)
-        
-    print("random keys:", random_keys)
+        print_tests(key, indent, level)
+        print("----------------")
 
 
 main()
